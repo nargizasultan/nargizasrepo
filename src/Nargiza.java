c1edfaeb833c3dd49d604a320a58025f7836204e
elnurakaparova1 <elnura.kaparova99@gmail.com>
2022-07-05 19:30:07 +0600

    solving merge conflict

--- src/Nargiza.java
+++ src/Nargiza.java
@@ -2,5 +2,14 @@ public class Nargiza {
     public static void nargiza(){
         System.out.println
                 ("Hello my name is nargiza");
+
+
+
+
+
+
+
+
+        System.out.println("My name is Elnura");
     }
 }
